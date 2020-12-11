ea647ee80b2d190dd74788b55c2c3198a6bd870f
Jesús Alberto Reyes Gutiérrez <alberto.r@ciencias.unam.mx>
2020-12-11 01:19:03 -0600

    Modifica el método para leer gramáticas.
    Ahora el método devuelve una ChomskyGrammar en la que se codifica la
    información de la gramárica en la forma normal de Chomsky.

--- Práctica2/GrammarReader.java
+++ Práctica2/GrammarReader.java
@@ -1,144 +1,40 @@
-import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class GrammarReader {
 
-	/** Cadena que contiene el nombre del archivo */
-	private String archivo;
-
-	/**
-	 * Cadena que contiene la cadena que se analizara si la cadena pertenece o no al
-	 * lenguaje generado por la gramatica
-	 */
-	private String cadena;
-
-	/** Arreglo de cadenas que contiene las */
-	private char[] productions;
-
-	/** Arreglo de cadenas que contiene las */
-	private char[] coproduction1;
-
-	/** Arreglo de cadenas que contiene las */
-	private char[] coproduction2;
-
-	/**
-	 * Constructor
-	 */
-	public GrammarReader(String[] args) {
-		readChomsyGrammar(args);
-	}
-
 	/**
 	 * Método de lectura
-	 * 
-	 * @param args argumentos
+	 *
+	 * @param file argumentos
 	 */
-	public void readChomsyGrammar(String args[]) {
-		ArrayList<String> producciones = new ArrayList<String>();
-		if (args.length != 2) {
-			System.out.print(args.length + "Faltan argumentos\n");
-		} else {
-			this.archivo = args[0];
-			try {
-				FileReader f = new FileReader(archivo);
-				BufferedReader lector = new BufferedReader(f);
-				String linea;
-				while ((linea = lector.readLine()) != null) {
-					linea = linea.replace(" ", "");
-					linea = linea.trim();
-					producciones.add(linea);
-				}
-				lector.close();
-			} catch (Exception e) {
-				System.err.println("No se encontro el archivo");
+	public static ChomskyGrammar readChomskyGrammar(String file) {
+		ArrayList<String> productionsArray = new ArrayList<>();
+		try {
+			FileReader f = new FileReader(file);
+			BufferedReader lector = new BufferedReader(f);
+			String linea;
+			while ((linea = lector.readLine()) != null) {
+				productionsArray.add(linea);
 			}
-
-			this.cadena = args[1];
-			this.productions = new char[producciones.size()];
-			this.coproduction1 = new char[producciones.size()];
-			this.coproduction2 = new char[producciones.size()];
-
-
-			for (int i = 0; i < producciones.size(); i++) {
-				String aux = producciones.get(i);
-				aux = aux.substring(0, 1);
-				this.productions[i] = aux.charAt(0);
-				aux = producciones.get(i);
-				aux = aux.substring(3, aux.length());
-				if (aux.length() == 1) {
-					this.coproduction1[i] = aux.charAt(0);
-					this.coproduction2[i] = '0';
-				}
-				if (aux.length() == 2) {
-					this.coproduction1[i] = aux.charAt(0);
-					this.coproduction2[i] = aux.charAt(1);
-				}
-			}
-		}
-	}
-
-	/**
-	 * Regresa la cadena con el nombre del archivo.
-	 * 
-	 * @return archivo El nombre del archivo.
-	 */
-	public String getArchivo() {
-		return this.archivo;
-	}
-
-	/**
-	 * Regresa la cadena con la cadena que se analizará.
-	 * 
-	 * @return cadena La cadena que se analizará.
-	 */
-	public String getCadena() {
-		return this.cadena;
-	}
-
-	/**
-	 * Regresa el arreglo con las producciones.
-	 * 
-	 * @return productions El arreglo con las producciones.
-	 */
-	public char[] getProductions() {
-		return this.productions;
-	}
-
-	/**
-	 * Regresa el arreglo con las co-producciones.
-	 * 
-	 * @return productions El arreglo con las co-producciones.
-	 */
-	public char[] getCoproduction1() {
-		return this.coproduction1;
-	}
-
-	/**
-	 * Regresa el arreglo con las co-producciones.
-	 * 
-	 * @return productions El arreglo con las co-producciones.
-	 */
-	public char[] getCoproduction2() {
-		return this.coproduction2;
-	}
-
-	/**
-	 * Regresa la cadena con las producciones y co-producciones respectivamente.
-	 * 
-	 * @return cadena La cadena con las producciones y co-producciones
-	 *         respectivamente.
-	 */
-	public String aCadena() {
-
-		String cadena = "";
-
-		for (int i = 0; i < productions.length; i++) {
-			cadena += "Produccion: " + productions[i] + ", coproduccion1: " + coproduction1[i] + ", coproduccion2: " + coproduction2[i] + "\n";
+			lector.close();
+		} catch (Exception e) {
+			System.err.println("Error: No se encontró el archivo");
 		}
 
-		return cadena;
+		int size = productionsArray.size();
+		char[] productions = new char[size];
+		char[] coproduction1 = new char[size];
+		char[] coproduction2 = new char[size];
+		for (int i = 0; i < size; i++) {
+			productions[i] = productionsArray.get(i).charAt(0);
+			coproduction1[i] = productionsArray.get(i).charAt(3);
+			coproduction2[i] = productionsArray.get(i).charAt(4);
+		}
 
+		return new ChomskyGrammar(productions, coproduction1, coproduction2);
 	}
+
 }
