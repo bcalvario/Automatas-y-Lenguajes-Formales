d76a4ba3cf2a050405c7245b867755707f4d8d6b
bcalvario <bcalvario@ciencias.unam.mx>
2020-12-10 21:28:20 -0600

    Se cambiaron tipos de datos en GrammarReader

--- Práctica2/ChomsyGrammar.java
+++ Práctica2/ChomsyGrammar.java
@@ -1,18 +1,29 @@
+import java.util.HashSet;
+
 public class ChomsyGrammar {
-	Char[] productions;
+	char[] productions;
 	String[] coproduction;
+	
 	// Ambos arreglos deben tener la misma longitud.
 	// La entrada i de productions deriva en la entrada i de coproductions.
 	// La entrada 0 contiene la prosucción inicial
 
-	public HashSet[][] getDerivationTableof(String word) {
-		HashSet[][] derivationTable = new HashSet[][]
+	//public HashSet[][] getDerivationTableof(String word) {
+	public void getDerivationTableof(String word) {
+	
+		HashSet[][] derivationTable = new HashSet[1][1];
 	}
 
-	public HashSet<String>[][] derivationTableRec(int i, 
-		                             HashSet<String>[][] tableBefore) {
+	//public HashSet<String>[][] derivationTableRec(int i, HashSet<String>[][] tableBefore) {
+	public void derivationTableRec(int i, HashSet<String>[][] tableBefore) {
+	
 		if (i == tableBefore.length) {
 			
 		}
+	}
+
+	public static void main (String[] args) {
+		GrammarReader gram = new GrammarReader(args);
+		System.out.println(gram.aCadena());
 	}
 }
