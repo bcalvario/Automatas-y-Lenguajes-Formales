4c4a54213c00b758e42bc675126a53409a0ed149
Jesús Alberto Reyes Gutiérrez <alberto.r@ciencias.unam.mx>
2020-12-10 23:16:06 -0600

    Implementa el método getDerivationTableOf.
    El método recibe una palabra y devuelve su tabla de derivación respecto
    al algoritmo CKY.

--- Práctica2/ChomsyGrammar.java
+++ Práctica2/ChomsyGrammar.java
@@ -2,51 +2,49 @@ import java.util.HashSet;
 
 public class ChomsyGrammar {
 	char[] productions;
-	char[] coproduction1;
-	char[] coproduction2;
+	char[] coproduct1;
+	char[] coproduct2;
 
-	// Ambos arreglos deben tener la misma longitud.
-	// La entrada i de productions deriva en la entrada i de coproductions.
-	// La entrada 0 contiene la prosucción inicial
-
-	public ChomsyGrammar (char[] productions, char[] coproduction1, char[] coproduction2) {
+	public ChomsyGrammar (char[] productions, char[] coproduct1, char[] coproduct2) {
 		this.productions = productions;
-		this.coproduction1 = coproduction1;
-		this.coproduction2 = coproduction2;
+		this.coproduct1 = coproduct1;
+		this.coproduct2 = coproduct2;
 	}
 
-	public boolean hasRigthChar (char character, int i) {
-		return coproduction2[i] == character;
+	public boolean hasRightChar (char character, int i) {
+		return coproduct2[i] == character;
 	}
 
-	public HashSet[][] getDerivationTableof(String word) {
+	public HashSet<Character>[][] getDerivationTableOf(String word) {
 		int length = word.length();
 
-		//Inicializar el arreglo de escalera
-		HashSet<Character>[][] derivationTable = new HashSet[length][];
+		HashSet[][] derivationTable = new HashSet[length][];
 		for (int i = 0; i < length; i++) {
-			derivationTable[i] = new HashSet[length - i];
+			derivationTable[i] = new HashSet[i+1];
 		}
 
-		// Subcadenas de longitud 1
 		for (int i = 0; i < length; i++) {
 			derivationTable[i][i] = new HashSet<>();
 			for (int j = 0; j < productions.length; j++) {
-				if (hasRigthChar(word.charAt(i), j))
+				if (hasRightChar(word.charAt(i), j))
 					derivationTable[i][i].add(productions[j]);
+			}
+		}
+
+		for (int m = 2; m <= length; m++) {
+			for (int i = 0; i <= length-m; i++) {
+				derivationTable[i+m-1][i] = new HashSet<>();
+				for (int j = i+1; j < i+m; j++) {
+					for (int k = 0; k < productions.length; k++) {
+						if (derivationTable[j-1][i].contains(coproduct1[k]) &&
+								derivationTable[i+m-1][j].contains(coproduct2[k]))
+							derivationTable[i+m-1][i].add(productions[k]);
+					}
+				}
 			}
 		}
 
 		return  derivationTable;
 	}
-
-	public static void main(String[] args) {
-		//Ejemplo de gramática en forma de Chamsky
-		char[] producciones0 = {'S','S','S','S','S','A','B','C','D'};
-		char[] prosucciones1 = {'A','B','S','A','B',' ',' ','S','S'};
-		char[] producciones2 = {'B','A','S','C','D','a','b','B','A'};
-		ChomsyGrammar grammar = new ChomsyGrammar(producciones0, prosucciones1, producciones2);
-
-		grammar.getDerivationTableof("abba");
-	}
+	
 }
