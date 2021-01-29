2c863b6967056b0e8fe4b0b8836a22f3736c7e1c
Berenice Calvario <bcalvario@ciencias.unam.mx>
2021-01-29 08:12:40 -0600

    Se creo la base para la práctica 3.

---
+++ Practica3/README.md
@@ -0,0 +1,1 @@
+sudo apt install libgoogle-gson-java
---
+++ Practica3/src/main/java/mx/unam/ciencias/alf/practica3/Practica3.java
@@ -0,0 +1,20 @@
+import java.io.FileReader;
+import com.google.gson.Gson;
+
+/**
+ * @author Berenice Calvario Gonzalez
+ * @author Jesús Alberto Reyes Gutiérrez
+ * @param args
+ * @throws java.io.IOException
+ */
+
+
+public class Practica3 {
+    public static void main(String args[]) throws java.io.IOException {
+
+        Gson g = new Gson();
+
+        System.out.println("Hola");
+    }
+ 
+}
