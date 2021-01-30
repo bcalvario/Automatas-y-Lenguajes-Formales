2340cd34a1e0635adc68b99b906774ad5c18fdeb
Berenice Calvario <bcalvario@ciencias.unam.mx>
2021-01-30 02:20:32 -0600

    The classes Tuple and Triplet were created.

---
+++ Practica3/Practica3.java
@@ -0,0 +1,17 @@
+package mx.unam.ciencias.alf.practica3;
+
+/**
+ * @author Berenice Calvario Gonzalez
+ * @author Jesús Alberto Reyes Gutiérrez
+ * @param args
+ */
+
+public class Practica3 {
+
+    public static void main(String[] args) {
+
+        Tuple tuple = new Tuple("q0", '0');
+        
+        Triplet triplet = new Triplet("q0", '0', 'R');
+    }
+}
