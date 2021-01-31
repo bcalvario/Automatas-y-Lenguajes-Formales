5081118fdae236783b500e1606d4d07b78666951
Berenice Calvario <bcalvario@ciencias.unam.mx>
2021-01-30 21:42:04 -0600

    Se modificó el main.

---
+++ Practica3/src/main/java/mx/unam/ciencias/alf/practica3/MTReader.java
@@ -0,0 +1,30 @@
+package mx.unam.ciencias.alf.practica3;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * Esta clase provee de las herramientas necesarias para leer un archivo tipo
+ * JSON y convertirlo en un objeto tipor MaquinaDeTuring.
+ */
+public class MTReader {
+
+    public static MaquinaDeTuring readTM(String file) {
+        String json = "";
+        try {
+            FileReader f = new FileReader(file);
+            BufferedReader lector = new BufferedReader(f);
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                json += linea;
+            }
+            lector.close();
+        } catch (Exception e) {
+            System.err.println("No se encontró el archivo");
+        }
+        Gson gson = new Gson();
+        return gson.fromJson(json, MaquinaDeTuring.class);
+    }
+}
---
+++ Practica3/src/main/java/mx/unam/ciencias/alf/practica3/Main.java
@@ -0,0 +1,25 @@
+package mx.unam.ciencias.alf.practica3;
+
+import com.google.gson.Gson;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print("Archivo con la descripcion de la MT : ");
+        String cadena = scanner.next();
+        MaquinaDeTuring mt = MTReader.readTM(cadena);
+
+        System.out.print("Inserte la cadena de entrada : ");
+        cadena = scanner.next();
+        mt.imprimeEjecucion(cadena);
+
+        scanner.close();
+
+    }
+
+}
